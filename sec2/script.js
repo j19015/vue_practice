505318e7e5ae9bbdd4f0b9e486d3e14402570f6e
j19015 <j19015@sangi.jp>
2022-03-02 22:03:04 +0900

    sec2.9

--- sec2/script.js
+++ sec2/script.js
@@ -1,3 +1,10 @@
-const app=Vue.createApp({
+//const app=Vue.createApp({
     //options
-})
+//})
+//mountメソッドを利用することで指定したHTML要素がVue.jsが用意したDOMに置き換わる
+//この操作をmountという
+//app.mount('#app')
+
+const app=Vue.createApp({
+    options
+}).mount('#app')
