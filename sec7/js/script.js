b472933de578b96b2ec0b385dce3e0fc0215009b
j19015 <j19015@sangi.jp>
2022-03-28 13:14:38 +0900

    sec7-58

--- sec7/js/script.js
+++ sec7/js/script.js
@@ -1,11 +1,20 @@
 const app=Vue.createApp({
     data:()=>({
-        message: 'Hello Vue.js!'
+      km:0,
+      m:0 
     }),
     watch:{
-        message: function(newValue,oldValue){
-            console.log('new:%s,old:%s',newValue,oldValue)
+        km:function(value){
+            console.log(value)
+            //this.km=value
+            this.m=value*1000
+        },
+        m:function(value){
+            console.log(value)
+            //this.m=value
+            this.km=value/1000
         }
     }
+
 })
 app.mount('#app')
